0406f2e61d44f87dc93690104f35b67e90430264
gir489returns <gir489returns@outlook.com>
2024-08-11 19:21:57 -0400

    Incorporate eThreadState changes.

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        b9b832ab00c95a731f8472f696c5d026a29fd767
+    GIT_REPOSITORY https://github.com/gir489returns/GTAV-Classes.git
+    GIT_TAG        edb71876ab8eee1e2663eb26c06bf6905c5d3934
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
