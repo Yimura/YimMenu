e834524ab4a8d2b5373cd9fce19985259171ddcf
gir489returns <gir489returns@outlook.com>
2024-08-13 18:34:04 -0400

    Redesigned delete_vehicle protections.

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        b9b832ab00c95a731f8472f696c5d026a29fd767
+    GIT_REPOSITORY https://github.com/gir489returns/GTAV-Classes.git
+    GIT_TAG        d700a082cbca28e2ca8a0251e3663c582a24b276
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
